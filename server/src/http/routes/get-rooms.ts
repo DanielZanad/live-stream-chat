15a39e3cda50eb706e5f168319f6bb2621135d45
Daniel Zanad <daniel.zanad.dz@gmail.com>
2025-07-11 16:50:25 -0300

    add create questions and get room questions

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -1,6 +1,7 @@
 import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { schema } from "../../db/schema/index.ts";
 import { db } from "../../db/connection.ts";
+import { eq, count } from "drizzle-orm";
 
 export const getRoomsRoute: FastifyPluginAsyncZod = async (app) => {
   app.get("/rooms", async () => {
@@ -8,8 +9,12 @@ export const getRoomsRoute: FastifyPluginAsyncZod = async (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        createdAt: schema.rooms.createdAt,
+        questionCount: count(schema.questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt);
 
     return results;
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import {
 import { env } from "./env.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoom } from "./http/routes/create-room.ts";
+import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.register(fastifyCors, {
@@ -23,6 +25,8 @@ app.get("/health", () => {
 
 app.register(getRoomsRoute);
 app.register(createRoom);
+app.register(getRoomQuestions);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("server running on http://localhost:3333");
